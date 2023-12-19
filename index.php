a1d13d9693c2b59078da16afc3817eb2bc2be114
emilienbt <neboutemilie@gmail.com>
2023-12-19 14:47:35 +0100

    page header
    suprr la page header

--- index.php
+++ index.php
@@ -17,7 +17,39 @@
 
 <body>
     <!-- Navbar -->
-    <?php include("./php/header.php"); ?>
+    <nav class="navbar navbar-expand-lg fixed-top">
+        <div class="container">
+ 
+     
+        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
+                aria-labelledby="offcanvasNavbarLabel">
+                <div class="offcanvas-header">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel">LogoHAHAHAH</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
+                <div class="offcanvas-body">
+                    <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
+                     
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="../poo/inscription.php">Inscription</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="#">Partenaire</a>
+                        </li>
+                        <li class="nav-item">
+                     <div class="logo">   </div>
+                        </li>
+                        
+                        <li class="nav-item">
+                            <a class="nav-link mx-lg-2" href="/webweek/php/equipe.php">Equipe</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link mx-lg-2" href="/webweek/php/resultat.php">Classement</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            
             <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
                 aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
