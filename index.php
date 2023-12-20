215d2e3db15e3762178fb8d84196591fdfacbddf
emilienbt <neboutemilie@gmail.com>
2023-12-20 15:44:23 +0100

    page accueil
    petits détails

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="fr">
 
@@ -8,10 +7,9 @@
     <title>Accueil</title>
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
-        <link href="css/style.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
 
 </head>
 
@@ -19,19 +17,18 @@
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
- 
-     
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar"
-                aria-labelledby="offcanvasNavbarLabel">
+
+
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
                 <div class="offcanvas-header">
                     <h5 class="offcanvas-title" id="offcanvasNavbarLabel">LogoHAHAHAH</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
                 <div class="offcanvas-body">
                     <ul class="navbar-nav justify-content-center align-items-center flex-grow-1 pe-3">
-                        
-                    <li class="nav-item">
-                     <div class="logo"><a href="../index.php"></a></div>
+
+                        <li class="nav-item">
+                            <div class="logo"><a href="../index.php"></a></div>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link mx-lg-2" href="poo/inscription.php">Inscription</a>
@@ -43,20 +40,19 @@
                             <a class="nav-link mx-lg-2" href="php/equipe.php">Equipe</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link mx-lg-2" href="php/resultat.php">Classement</a>
+                            <a class="nav-link mx-lg-2" href="php/resultat.php">Classement</a>
                         </li>
                     </ul>
                 </div>
             </div>
 
-            <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar"
-                aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <button class="navbar-toggler pe-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </div>
     </nav>
 
-<div class="ahah"> </div>
+    <div class="ahah"> </div>
     <!-- Hero Section -->
 
     <section class="jumbotron jumbotron-fluid jumbotron-custom">
@@ -67,120 +63,84 @@
             </div>
         </div>
     </section>
-    <br><br>
     <div class="evenement1">
-
-    <section class="section-margin">
-        <div class="container text-center text-white">
-          <h2 class="pb-5">Information</h2>
-          <div class="row">
-            <div class="col">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 002.25-2.25V6.75A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25v10.5A2.25 2.25 0 004.5 19.5z"
-                />
-              </svg>
-              <hr />
-              <p class="explication-titre ">Ou ?</p>
-              <p class="explication-text">
-              Aux Estables, a proximite des piste de ski, des parcours en un minimum de temps avec ton equipe.
-              </p>
-            </div>
-            <div class="col">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-                />
-              </svg>
-              <hr />
-              <p class="explication-titre ">Quand ?</p>
-              <p class="explication-text">
-              Le 14 decembre tout au long de la journee. Vous retrouverez la programmation complete ainsi que les horaires.
-Inscription
-              </p>
-            </div>
-            <div class="col">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="icon"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z"
-                />
-              </svg>
-              <hr />
-              <p class="explication-titre ">Evenement ?</p>
-              <p class="explication-text">
-              une aventure sportive et festive qui rechauffera les coeurs en plein hiver
-        <!-- Colonne "QUAND ?" -->
-        <div class="col-md-4 mb-3">
-            <div class="card custom-card">
-                <div class="card-body">
-                    <p class="card-text">Le 14 décembre tout au long de la journée. Vous retrouverez la programmation complète ainsi que les horaires.</p>
-                    <a href="poo/inscription.php" class="stretched-link">Inscription</a>
-                </div>
-            </div>
-          </div>
+        <br><br>
+        <section class="section-margin">
+            <div class="container text-center text-white">
+                <h2 class="pb-5">Information</h2>
+                <div class="row">
+                    <div class="col">
+                        <img src="./img/emplacement.png" class="icon"><br><br>
+                        <p class="explication-titre ">Où ?</p>
+                        <p class="explication-text">
+                            Aux Estables, a proximite des piste de ski, des parcours en un minimum de temps avec ton equipe.
+                        </p>
+                    </div>
+                    <div class="col">
+                    <img src="./img/calendrier.png" class="icon"><br><br>
+                        <p class="explication-titre ">Quand ?</p>
+                        <p class="explication-text">
+                            Le 14 Décembre tout au long de la journee. Vous retrouverez la programmation complete ainsi que les horaires.
+                        </p>
+                    </div>
+                    <div class="col">
+                    <img src="./img/ballon-de-foot.png" class="icon"><br><br>
+                        <p class="explication-titre ">Évènement ?</p>
+                        <p class="explication-text">
+                            Une aventure sportive et festive qui rechauffera les coeurs en plein hiver </p>
+                    </div>
+        </section>
+    </div>
+    <section class="Avertissement">
+        <br><br>
+        <strong>Le nombre de place étant limité, nous vous invitons à réserver pour votre équipe de 6 personnes au plus vite.</strong>
+        <br><br><br>
+        <div class="Gauche">
+            <h4>ACCES</h4>
+            <p>Liaisons françaises :<br> Le Puy en Velay : 33 km</p>
         </div>
-      </section>
-</div>
-</div>
-<br>
-<h2 class="text-center">ACTIVITES</h2>
-<br>
-<div id="carouselpuy" class="carousel slide" data-bs-ride="carousel">
+        <div class="Droite">
+            <h4>SUR PLACE</h4>
+            <p>Buvette et snack tout au long de la journée. <br>A proximité des hôtels et gites.</p>
+        </div>
+        <br><br><br>
+        <p id="gris">L’association décline toute responsabilité en cas de vol !</p>
+        <br>
+        <button id="jeminscris">JE M'INSCRIS</button>
+    </section>
+    </div>
+    <br>
+    <h2 class="text-center">ACTIVITES</h2>
+    <br>
+    <div id="carouselpuy" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img src="img/curlingdalee.png" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="img/hockeydalle.png" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="img/lugedalle.png" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="img/paintballdalee.png" class="d-block w-100" alt="...">
-          </div>
-          <div class="carousel-item">
-            <img src="img/skidalle.png" class="d-block w-100" alt="...">
-          </div>
+            <div class="carousel-item active">
+                <img src="img/curlingdalee.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/hockeydalle.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/lugedalle.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/paintballdalee.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="img/skidalle.png" class="d-block w-100" alt="...">
+            </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselpuy" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Previous</span>
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselpuy" data-bs-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="visually-hidden">Next</span>
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
         </button>
-      </div>
-      <br><br><br>
-<div id="ancre_programme" class="deroulement">
+    </div>
+    <div id="ancre_programme" class="deroulement">
+    <br><br><br>
         <h2 class="text-center">DEROULEMENT DE LA JOURNEE</h2>
         <br><br>
         <div class="container_horaires">
@@ -211,8 +171,7 @@ Inscription
         </div>
     </div>
     <!-- Boostrap JS -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
 </body>
+
 </html>
